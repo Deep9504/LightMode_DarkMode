b0b62e1e3a789f37098a1d41a9a68e2fb02a9a35
Deepshikha singh <87688252+Deep9504@users.noreply.github.com>
2024-04-22 13:33:55 +0530

    Add files via upload
    Created a Light And Dark Mode Theme Page.

---
+++ src/pages/blog.js
@@ -0,0 +1,9 @@
+const Blog = () => {
+    return (
+        <div className="page">
+            <h1>Blog Page</h1>
+            <p>Read out our latest Blog Here</p>
+        </div>
+    );
+};
+export default Blog;
